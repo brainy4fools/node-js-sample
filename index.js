cdc3336024db739bdce82895bac98c886cfc2495
brainy4fools <59425456+brainy4fools@users.noreply.github.com>
2020-06-10 14:46:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World new!')
+  response.send('Hello World newr!')
 })
 
 app.listen(app.get('port'), function() {
