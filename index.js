5636a7b0a691576cde51112639c6ff0f5436fe7b
brainy4fools <59425456+brainy4fools@users.noreply.github.com>
2020-06-10 14:50:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = new express();
 
 app.get('/', function(request, response){
-    response.sendFile('./index.html');
+    response.sendFile('index.html');
 });
