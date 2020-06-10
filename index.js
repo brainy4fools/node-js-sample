1360da83b75810e72c71585a044ede3d2d795cc0
brainy4fools <59425456+brainy4fools@users.noreply.github.com>
2020-06-10 14:51:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express');
-const app = new express();
+var http = require('http'),
+    fs = require('fs');
 
-app.get('/', function(request, response){
-    response.sendFile('index.html');
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
